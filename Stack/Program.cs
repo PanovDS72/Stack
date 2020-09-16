0a9beae5ef60793823bdb831b159bad57d174a68
Denis <panovds72@gmail.com>
2020-09-16 16:23:28 +0500

    Add project files.

---
+++ Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);            
+           
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());            
+
+            Console.ReadLine();
+        }
+    }
+}
